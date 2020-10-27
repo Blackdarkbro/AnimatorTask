22efaf5035b8ab1b7fed0575eb91d087eaa90d9c
Egor Savchik <egorsa@playtika.com>
2020-10-27 13:08:07 +0300

    Initial commit

---
+++ Assets/AnimationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private Animator _animator;
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _animator.SetBool("Walk", Input.GetKey(KeyCode.W));
+        
+        _animator.SetBool("Crouch", Input.GetKey(KeyCode.S));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.SetTrigger("Jump");
+        }
+        
+    }
+}
