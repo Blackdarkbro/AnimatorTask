518a76e3f3464b1bda35e294beb6016fb7f3e245
Egor Savchik <egorsa@playtika.com>
2020-10-27 19:19:47 +0300

    Blend Tree
    uncreative

---
+++ Assets/ButtonController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ButtonController : MonoBehaviour
+{
+    private Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            _animator.SetFloat("Color", Random.Range(0f, 2f));
+            _animator.SetFloat("Scale", Random.Range(0f, 2f));
+        }
+    }
+}
